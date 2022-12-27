6f8782f08ad5d72370091dab48c4200230094c97
Joergen Tjoennteig <jorgenlt@outlook.com>
2022-12-27 14:39:22 +0800

    post destroy

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to root_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
