0e1d7060d4993b431e2d8307e8f44cf806d7cc42
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-01-09 20:55:06 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'faker'
+# require 'faker'
+
+# 10.times do
+#   Post.create(user_id: 1, title: "Lorem ipsum dolor", content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam optio adipisci, dolores illo quo quod quaerat laborum aliquam, temporibus voluptate officia hic quae, porro praesentium obcaecati. Voluptate optio ullam corrupti.")
+#   puts "Post created"
+# end
+
+10.times do
+  i = 1
+  10.times do
+    Comment.create(user_id: 2, post_id: i, content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam optio adipisci, dolores illo quo quod quaerat laborum aliquam, temporibus voluptate officia hic quae, porro praesentium obcaecati. Voluptate optio ullam corrupti.")
+    i += 1
+    puts "Comment created"
+  end
+end
