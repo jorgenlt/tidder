ac29c708ece38f6b6dabb9a956bfdb9e2733a199
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-01-13 16:31:09 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
 
-10.times do
+17.times do
   User.create(email: Faker::Internet.email, password: "123456", username: Faker::Internet.username(specifier: 5..8))
 end
 
 puts 'Users created.'
 
 i = 1
-10.times do
+17.times do
   Post.create(user_id: i, title: Faker::Quote.famous_last_words, content: Faker::Quote.matz)
   i += 1
 end
@@ -22,9 +22,9 @@ end
 puts 'Posts created.'
 
 postid = 1
-10.times do
+17.times do
   userid = 1
-  10.times do
+  7.times do
     Comment.create(user_id: userid, post_id: postid, content: Faker::Quote.matz)
     userid += 1
   end
@@ -32,3 +32,14 @@ postid = 1
 end
 
 puts 'Comments created.'
+
+# i = 1
+# 10.times do
+#   post = Post.find(i)
+#   user = 1
+#   10.times do
+#     post.upvote_by User.find(user)
+#     user += 1
+#   end
+#   i += 1
+# end
